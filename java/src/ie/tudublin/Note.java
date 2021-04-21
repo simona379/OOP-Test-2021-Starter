75a3cd5aa15a01e208ab0bae53b8507cf59d11e5
Simona Petrauskaite <simonapetrauskaite@simonas-mbp.home>
2021-04-21 16:54:45 +0100

    added getters and setters

--- java/src/ie/tudublin/Note.java
+++ java/src/ie/tudublin/Note.java
@@ -5,7 +5,24 @@ public class Note {
     private char note;
     private int duration;
 
-    //Constructor (not default)
+    //Getters & Setters for note and duration
+    public char getNote() {
+        return note;
+    }
+
+    public void setNote(char note) {
+        this.note = note;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+
+    //Constructor (no default constructor needed)
     public Note(char note, int duration) {
         this.note = note;
         this.duration = duration;
