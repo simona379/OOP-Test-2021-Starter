2efa0ba5ba32869665946fe521a826919e2df8b2
Simona Petrauskaite <simonapetrauskaite@simonas-mbp.home>
2021-04-21 16:44:28 +0100

    added constructor and toString() method

--- java/src/ie/tudublin/Note.java
+++ java/src/ie/tudublin/Note.java
@@ -4,6 +4,18 @@ public class Note {
 
     private char note;
     private int duration;
+
+    //Constructor (not default)
+    public Note(char note, int duration) {
+        this.note = note;
+        this.duration = duration;
+    }
+
+    //toString() method
+    @Override
+    public String toString() {
+        return "Note [duration=" + duration + ", note=" + note + "]";
+    }
     
     
 }
